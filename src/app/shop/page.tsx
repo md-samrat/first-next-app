0ee9619cfac2eff42187cb82a89533eb99a6c117
samrat-xyz <md.samrat61790@gmail.com>
2026-02-24 01:14:07 +0600

    implement dynamic nested  routes

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -5,7 +5,7 @@ function Shop() {
     <div>
         <h1 className='text-xl font-semibold'>Shop Page</h1>
         <p>Categories:</p>
-        <h3>Example</h3>
+        <h3>Example :</h3>
         <ul className='flex items-center gap-8 list-disc'>
             <li>Phone</li>
             <li>Laptop</li>
