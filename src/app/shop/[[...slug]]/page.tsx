abe4e7ee792955ab0a8f4f190f422e50612ca737
samrat-xyz <md.samrat61790@gmail.com>
2026-02-24 02:13:18 +0600

    implement catch all segments and optional catch all segments

---
+++ src/app/shop/[[...slug]]/page.tsx
@@ -0,0 +1,16 @@
+
+async function Products({params}:{params:Promise<{slug:string[]}>}) {
+  const {slug} = await params
+  return (
+    <div>
+      <h1>Products</h1>
+      <div>
+        {
+          slug?.length > 0 && slug.map((item,idx)=><p key={idx}>{item}</p>)
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Products
